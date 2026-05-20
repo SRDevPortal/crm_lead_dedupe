16eb3f9b1931b9c665afd8cf28d6fbc16ee40978
Sriaas Dev <webdevelopersriaas@gmail.com>
2026-05-20 19:27:11 +0530

    Remove obsolete mobile normalization patches

--- crm_lead_dedupe/patches/v15_add_mobile_pipeline_index.py
+++
@@ -1,9 +0,0 @@
-import frappe
-
-def execute():
-    try:
-        frappe.db.add_index("CRM Lead",
-                            ["sr_mobile_norm", "sr_lead_pipeline"],
-                            index_name="idx_crmlead_mobile_pipe")
-    except Exception:
-        pass
