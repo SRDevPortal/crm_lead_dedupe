2c2518e4c54704ee04664084353e78d003de8562
Sriaas Dev <webdevelopersriaas@gmail.com>
2026-05-22 18:31:58 +0530

    Refactor CRM lead dedupe setup initialization

--- crm_lead_dedupe/setup/__init__.py
+++ crm_lead_dedupe/setup/__init__.py
@@ -1,3 +1,25 @@
-from .crm_lead_cf import DT, CUSTOM_FIELDS, apply, backfill_mobile_norm, ensure_indexes
+from .crm_lead_cf import (
+    CUSTOM_FIELDS,
+    DT,
+    apply,
+    backfill_mobile_norm,
+    backfill_mobile_norm_batched,
+    clear_legacy_duplicate_links_batched,
+    ensure_indexes,
+    reset_backfill_progress,
+    run_backfill_batch,
+    sync_duplicate_groups_batched,
+)
 
-__all__ = ["DT", "CUSTOM_FIELDS", "apply", "backfill_mobile_norm", "ensure_indexes"]
+__all__ = [
+    "CUSTOM_FIELDS",
+    "DT",
+    "apply",
+    "backfill_mobile_norm",
+    "backfill_mobile_norm_batched",
+    "clear_legacy_duplicate_links_batched",
+    "ensure_indexes",
+    "reset_backfill_progress",
+    "run_backfill_batch",
+    "sync_duplicate_groups_batched",
+]
